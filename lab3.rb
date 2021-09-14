ace55b5995e3c2dadc344dc6f4b5caa1b252bb09
陈劲安 <79066012@buaa.edu.cn>
2021-09-14 21:49:17 +0800

    check #4

--- lab3.rb
+++ lab3.rb
@@ -1,52 +1,28 @@
 def getDecode(s)
     n = s.size()
-    dp = Array.new(n, 0)
+    dp = Array.new(n + 1, 0)
     
-    if (s[0] == '0') 
-        return 0
-    end
-    dp[0] = 1
-
-    if (n == 1) 
-        return 1
+    dp[n] = 1
+    if (dp[n - 1] != '0') 
+        dp[n - 1] = 1
     end
 
-    x = s[0,2].to_i
-    if (0 <= x and x <= 26 and s[1] != '0') 
-        dp[1] = 2
-    elsif (0 <= x and x <= 26 and s[1] == '0') 
-        dp[1] = 1
-    elsif (s[1] != '0') 
-        dp[1] = 1
-    else 
-        dp[1] = 0
-    end
-
-    for i in 2...n
-        if (s[i] == '0') 
-            if (s[i - 1] == '0')
-                return 0
+    for i in (n - 2).downto(0)
+        if (s[i] != '0') 
+            x = s[i,2].to_i
+            if (1 <= x and x <= 26) 
+                dp[i] += dp[i + 2]
             end
-            x = s[i - 1,2].to_i
-            if (0 <= x and x <= 26) 
-                dp[i] = dp[i - 2]
-            end
-        else
-            dp[i] = dp[i - 1]
-            x = s[i - 1,2].to_i
-            if (0 <= x and x <= 26 and s[i - 1] != '0') 
-                dp[i] += dp[i - 2]
-            end
+            dp[i] += dp[i + 1]
         end
     end
 
-    return dp[n - 1]
+    return dp[0]
 end
 
 def testdecode
     for i in 1..5000
         s = i.to_s
-        # puts s
         puts getDecode(s)
     end
 end
