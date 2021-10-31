a1f6ebad982e0b64e418099566da66c5cf9a82a2
Alvintan0712 <tjan2000@gmail.com>
2021-11-01 00:08:41 +0800

    deploy

--- blog4/config/routes.rb
+++ blog4/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :comments
   root 'blogs#index'
-  resources :blogs
+  resources :blogs do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
