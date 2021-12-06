3bf0e57a8590209f2fc28a560d2e07d44bb22498
Alvintan0712 <tjan2000@gmail.com>
2021-12-06 16:25:44 +0800

    change attr_writer to attr_accessor

--- project/app/models/user.rb
+++ project/app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   # only allow letter, number, underscore and punctuation.
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
 
-  attr_writer :login
+  attr_accessor :login
 
   def login
     @login || self.username || self.email
