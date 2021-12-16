9f8893f4eae7983ce6e41b7265df536953360db4
Alvintan0712 <tjan2000@gmail.com>
2021-12-16 21:44:15 +0800

    impl change password WIP

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   get 'home/index'
+  get 'users/update_password'
 
   root to: 'home#index'
 end
