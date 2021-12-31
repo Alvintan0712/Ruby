ce667482857f78ced7e43766264b67f960efcc4a
Alvintan0712 <tjan2000@gmail.com>
2021-12-31 23:13:35 +0800

    edit environment

--- project/config/environments/development.rb
+++ project/config/environments/development.rb
@@ -74,5 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'ruby-git.act.buaa.edu.cn', port: 5000 }
+  config.hosts << "ruby-git.act.buaa.edu.cn"
 end
